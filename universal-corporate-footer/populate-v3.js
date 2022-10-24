8f87b049f55021872802d289e4be42a322ad85e6
Andreas Kournoutas <andreaskournoutas@gmail.com>
2022-10-24 08:41:14 +0300

    v3

--- universal-corporate-footer/populate-v3.js
+++ universal-corporate-footer/populate-v3.js
@@ -1,9 +1,7 @@
 const container = document.getElementById('ucf-container');
 
 if (container != null) {
-    const shadow = container.attachShadow({mode: 'open'});
-
     fetch('https://cdn.jsdelivr.net/gh/Alpha-Editions/internal-implementations@main/universal-corporate-footer/template.html').then(function(response) {
-        shadow.innerHTML = response;
+        shadow.innerHTML = response.text();
     });
 }
