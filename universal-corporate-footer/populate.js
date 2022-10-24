e6c8bfac3b5d7608ff77a49ded8df9570e330a7a
Andreas Kournoutas <andreaskournoutas@gmail.com>
2022-10-24 08:12:33 +0300

    Update populate.js

--- universal-corporate-footer/populate.js
+++ universal-corporate-footer/populate.js
@@ -3,7 +3,7 @@ const container = document.getElementById('ucf-container');
 if (container != null) {
     const shadow = container.attachShadow({mode: 'open'});
 
-    fetch('template.html').then(function(response) {
+    fetch('https://cdn.jsdelivr.net/gh/Alpha-Editions/internal-implementations@main/universal-corporate-footer/template.html').then(function(response) {
         shadow.innerHTML = response;
     });
 }
