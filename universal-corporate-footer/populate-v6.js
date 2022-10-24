c584b3a69fbea1613ad457ef17ff7f31f51d0cf7
Andreas Kournoutas <andreaskournoutas@gmail.com>
2022-10-24 09:19:58 +0300

    v6

--- universal-corporate-footer/populate-v6.js
+++ universal-corporate-footer/populate-v6.js
@@ -1,63 +1,58 @@
 const ucf = `
     <div id="ucf">
         <style>
-            *, *::before, *::after {
+            #ucf *, #ucf *::before, #ucf *::after {
                 box-sizing: border-box;
             }
-            a {
+            #ucf a {
                 display: flex;
                 justify-content: center;
                 align-self: center;
                 width: 100%;
             }
-            img {
+            #ucf img {
                 max-width: 100%;
             }
-            .row {
+            #ucf .row {
                 display: flex;
                 flex-wrap: wrap;
             }
-            .row > * {
+            #ucf .row > * {
                 flex-shrink: 0;
                 width: 100%;
                 max-width: 100%;
                 padding: 1rem;
                 display: flex;
             }
-            .col-6 {
+            #ucf .col-6 {
                 flex: 0 0 auto;
                 width: 50%;
             }
-            .col-12 {
+            #ucf .col-12 {
                 flex: 0 0 auto;
                 width: 100%;
             }
-            .w-75 {
+            #ucf .w-75 {
                 width: 75%;
             }
             @media (min-width: 992px) {
-                .col-lg-3 {
+                #ucf .col-lg-3 {
                     flex: 0 0 auto;
                     width: 25%;
                 }
-                .col-lg-9 {
+                #ucf .col-lg-9 {
                     flex: 0 0 auto;
                     width: 75%;
                 }
             }
-            @media (prefers-reduced-motion: no-preference) {
-                :root {
-                    scroll-behavior: smooth;
-                }
-            }
         </style>
         <div class="row">
-            <div class="col-12 col-lg-3" style="padding: 1.5rem 0;">
+            <div class="col-12 col-lg-3" style="padding: 1.5rem 2rem;">
                 <a target="_blank" rel="noopener">
-                    <img src="assets/alphaeditions.png" alt="">
+                    <img src="https://cdn.jsdelivr.net/gh/Alpha-Editions/internal-implementations@main/universal-corporate-footer/assets/alphaeditions.png" alt="" style="max-width: 300px;">
                 </a>
             </div>
-            <div class="col-12 col-lg-9" style="padding: 1.5rem 0 1.5rem 4.5rem;">
+            <div class="col-12 col-lg-9" style="padding: 1.5rem 2rem;">
                 <div class="row" style="justify-content: center;">
                     <div class="col-6 col-lg-3">
                         <a href="https://provocateur.gr/" target="_blank" rel="noopener">
